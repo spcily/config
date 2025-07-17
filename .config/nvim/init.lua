ee0b65a6cb997bb6e2e96b920cd3f553296cbb78
sp <spksk@protonmail.com>
2025-07-17 13:34:18 +0700

    update vim options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,18 +1,15 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+-- Set <space> as the leader key See `:help mapleader` NOTE: Must happen before
+-- plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+-- [[ Setting options ]] See `:help vim.o` NOTE: You can change these options
+-- as you wish! For more options, you can see `:help option-list`
 
 -- Make line numbers default
 vim.o.number = true
@@ -39,6 +36,12 @@ vim.o.breakindent = true
 
 -- Save undo history
 vim.o.undofile = true
+
+vim.opt.tabstop = 2 -- Number of spaces a <Tab> counts for
+vim.opt.softtabstop = 2 -- Number of spaces for <Tab> in insert mode
+vim.opt.shiftwidth = 2 -- Number of spaces for each indent level
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.smartindent = true -- Enable smart indentation
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
